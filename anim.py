552197f1d7bf29508bad252fa501178b81d7990c
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2021-04-18 09:10:29 +0200

    network prediction animation done

--- anim.py
+++ anim.py
@@ -3,10 +3,10 @@ from manim import *
 
 class AnimationMLDL(Scene):
     # config.background_color = WHITE
-    bottom_left_moves = [LEFT*5, DOWN*3]
-    bottom_right_moves = [RIGHT*5, DOWN*3]
-    bottom_center_right_moves = [RIGHT*1.5, DOWN*3]
-    bottom_center_left_moves = [LEFT*1.5, DOWN*3]
+    # bottom_left_moves = [LEFT*5, DOWN*3]
+    bottom_right_moves = [RIGHT*5, DOWN*3.5]
+    # bottom_center_right_moves = [RIGHT*1.5, DOWN*4]
+    # bottom_center_left_moves = [LEFT*1.5, DOWN*3]
     bottom_text_scale = 0.2
     text_source_scale = .25
 
@@ -17,7 +17,7 @@ class AnimationMLDL(Scene):
         self.intro()
         self.wait()
         self.pred_network()
-        self.wait()
+        self.wait(2)
 
         # text_polito = Text("Politecnico di Torino Apr 2021", size=self.bottom_text_scale)
         # # for move in self.bottom_right_moves:
@@ -53,11 +53,206 @@ class AnimationMLDL(Scene):
 
         # self.wait()
     def pred_network(self):
+        hidden_layer_height = 0
+        input_layer_height = -2.68
+        output_layer_height = 2.68
+        text_horizontal_position = -5.5
+        exp_text_horizontal_position = +1.5
+        arrows_scale = .35
+        
+        latex_text_scale = .6
+        surr_rect_buff = 0.05
+        surr_rect_stroke_width = 1.5
+        left_horizontal_latex_pos = -3.7
+        first_rnn= ImageMobject("/home/delta/Documents/Università/Machine Learning and Deep Learning/animation/first_rnn_orig_transparent_cropped.png")
+        first_rnn.scale(.8).set_color(WHITE)
+        self.play(FadeIn(first_rnn))
+        self.wait(2)
+
+        input_arrow = Arrow(np.array([-5, input_layer_height, 0]), np.array([-3, input_layer_height, 0]), buff=0, stroke_width=2)
+        VMobject.scale(input_arrow, arrows_scale)
+        input_text = Text("Input Layer").scale(.35).shift(UP*input_layer_height).shift(RIGHT*text_horizontal_position)
+        input_exp_text = Text("[0, ..., 1, 0, ...]").scale(.35).shift(UP*input_layer_height).shift(RIGHT*exp_text_horizontal_position)
+
+        hidden_arrow = Arrow(np.array([-5, hidden_layer_height, 0]), np.array([-3, hidden_layer_height, 0]), buff=0, stroke_width=2)
+        VMobject.scale(hidden_arrow, arrows_scale)
+        hidden_text = Text("Hidden Layers").scale(.35).shift(UP*hidden_layer_height).align_to(input_text, RIGHT)
+        hidden_exp_text = Text("LSTM cells").scale(.35).shift(UP*hidden_layer_height).align_to(input_exp_text, LEFT)
 
 
-        rnn_image = SVGMobject('/home/delta/Documents/Università/Machine Learning and Deep Learning/animation/rnn_image1.svg')
-        rnn_image.scale(rnn_image).set_color(WHITE)
-        rnn_image.shift(RIGHT*2)
+        output_arrow = Arrow(np.array([-5, output_layer_height, 0]), np.array([-3, output_layer_height, 0]), buff=0, stroke_width=2)
+        VMobject.scale(output_arrow, arrows_scale)
+        output_text = Text("Output Layer").scale(.35).shift(UP*output_layer_height).align_to(input_text, RIGHT)
+        output_exp_text = Text("Distribution of future inputs").scale(.35).shift(UP*output_layer_height).align_to(input_exp_text, LEFT)
+        
+        arrows = VGroup(
+                input_arrow,
+                hidden_arrow,
+                output_arrow
+            )
+        text = VGroup(
+            input_text,
+            hidden_text,
+            output_text
+        )
+        exp_text = VGroup(
+            input_exp_text,
+            output_exp_text
+        )
+        shift_horiz_tex = .5
+        big_hidden_exp_text = Text("LSTM cells").scale(.8).shift(UP*3)
+        lstm_cell_img= ImageMobject("/home/delta/Documents/Università/Machine Learning and Deep Learning/animation/lstm_cell_transparent.png")
+        lstm_cell_img.scale(.7).set_color(WHITE)
+
+        input_gate_tex = MathTex(r"i_t =",r"\sigma",r"(", r"W_{xi}", r"x_t", r"+",r"W_{hi}",r"h_{t-1}", r"+", r"W_{ci}", r"c_{t-1}", r"+", r"b_i", r")").scale(latex_text_scale).shift(RIGHT*left_horizontal_latex_pos)
+        function_gate_tex = MathTex(r"f_t =",r"\sigma",r"(", r"W_{xf}", r"x_t", r"+",r"W_{hf}",r"h_{t-1}", r"+", r"W_{cf}", r"c_{t-1}", r"+", r"b_f", r")").scale(latex_text_scale).align_to(input_gate_tex, LEFT).shift(DOWN*shift_horiz_tex)
+        output_gate_tex = MathTex(r"o_t =",r"\sigma",r"(", r"W_{xo}", r"x_t", r"+",r"W_{ho}",r"h_{t-1}", r"+", r"W_{co}", r"c_{t-1}", r"+", r"b_o", r")").scale(latex_text_scale).align_to(input_gate_tex, LEFT).shift(DOWN*shift_horiz_tex*2)
+        cell_gate_tex = MathTex(r"c_t =",r"i_t", r"tanh",r"(", r"W_{xi}", r"x_t", r"+",r"W_{hi}",r"h_{t-1}", r"+", r"b_i", r")").scale(latex_text_scale).align_to(input_gate_tex, LEFT).shift(DOWN*shift_horiz_tex*3)
+        hidden_gate_tex = MathTex(r"h_t = tanh(c_t)").scale(latex_text_scale).align_to(input_gate_tex, LEFT).shift(DOWN*shift_horiz_tex*4)
+
+        framebox_sigma_i = SurroundingRectangle(input_gate_tex[1], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_i_1 = SurroundingRectangle(input_gate_tex[3], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_i_2 = SurroundingRectangle(input_gate_tex[6], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_i_3 = SurroundingRectangle(input_gate_tex[9], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_i_1 = SurroundingRectangle(input_gate_tex[4], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_i_2 = SurroundingRectangle(input_gate_tex[7], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_i_3 = SurroundingRectangle(input_gate_tex[10], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_bias_i = SurroundingRectangle(input_gate_tex[-2], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        
+
+        framebox_sigma_f = SurroundingRectangle(function_gate_tex[1], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_f_1 = SurroundingRectangle(function_gate_tex[3], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_f_2 = SurroundingRectangle(function_gate_tex[6], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_f_3 = SurroundingRectangle(function_gate_tex[9], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_f_1 = SurroundingRectangle(function_gate_tex[4], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_f_2 = SurroundingRectangle(function_gate_tex[7], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_f_3 = SurroundingRectangle(function_gate_tex[10], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_bias_f = SurroundingRectangle(function_gate_tex[-2], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+
+        framebox_sigma_o = SurroundingRectangle(output_gate_tex[1], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_o_1 = SurroundingRectangle(output_gate_tex[3], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_o_2 = SurroundingRectangle(output_gate_tex[6], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_o_3 = SurroundingRectangle(output_gate_tex[9], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_o_1 = SurroundingRectangle(output_gate_tex[4], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_o_2 = SurroundingRectangle(output_gate_tex[7], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_o_3 = SurroundingRectangle(output_gate_tex[10], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_bias_o = SurroundingRectangle(output_gate_tex[-2], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+                
+        framebox_sigma_c = SurroundingRectangle(cell_gate_tex[2], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_c_1 = SurroundingRectangle(cell_gate_tex[4], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_weight_c_2 = SurroundingRectangle(cell_gate_tex[7], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_c_1 = SurroundingRectangle(cell_gate_tex[5], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_input_c_2 = SurroundingRectangle(cell_gate_tex[8], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+        framebox_bias_c = SurroundingRectangle(cell_gate_tex[-2], buff=surr_rect_buff, stroke_width=surr_rect_stroke_width)
+
+        math_text = VGroup(
+            input_gate_tex,
+            function_gate_tex,
+            output_gate_tex,
+            cell_gate_tex,
+            hidden_gate_tex
+        )
+
+        framebox_first_f = VGroup(
+            framebox_sigma_i,
+            framebox_sigma_f,
+            framebox_sigma_o,
+            framebox_sigma_c
+        )
+
+        framebox_bias = VGroup(
+            framebox_bias_i,
+            framebox_bias_f,
+            framebox_bias_o,
+            framebox_bias_c
+        )
+
+
+        framebox_input = VGroup(
+            framebox_input_i_1,
+            framebox_input_i_2,
+            framebox_input_i_3,
+            framebox_input_f_1,
+            framebox_input_f_2,
+            framebox_input_f_3,
+            framebox_input_o_1,
+            framebox_input_o_2,
+            framebox_input_o_3,
+            framebox_input_c_1,
+            framebox_input_c_2,
+        )
+
+        framebox_weight = VGroup(
+            framebox_weight_i_1,
+            framebox_weight_i_2,
+            framebox_weight_i_3,
+            framebox_weight_f_1,
+            framebox_weight_f_2,
+            framebox_weight_f_3,
+            framebox_weight_o_1,
+            framebox_weight_o_2,
+            framebox_weight_o_3,
+            framebox_weight_c_1,
+            framebox_weight_c_2,
+        )
+
+        code_scale = .41
+        code_ex_text = Text("Example for output gate during forward pass:").scale(.4).shift(2*UP).shift(3.7*RIGHT)
+        code_out_gate = Code(code="// output gate\n"+
+                            "// extra input from peephole (from current state)\n"+
+                            "#ifdef PEEPS\n"+
+                            "dot(stateBegin + cellStart, stateBegin + cellEnd, peepWtIt, inActIt,inActIt + 1);\n"+
+                            "peepWtIt += cellsPerBlock;\n"+
+                            "#endif\n"+
+                            "real_t outGateAct = G::fn(*inActIt);\n"+
+                            "outGateActBegin[b] = outGateAct;\n"+
+                            "++inActIt;\n\n"+
+                            "// output activations\n"+
+                            "transform(preOutGateActBegin + cellStart, preOutGateActBegin + cellEnd,\n"+
+                            "actBegin + cellStart,\n"+
+                            "bind2nd(multiplies<real_t>(), outGateAct));\n"+
+                            "cellStart = cellEnd;\n"+
+                            "cellEnd += cellsPerBlock;\n"+
+                            "fgActBegin = fgActEnd;\n"+
+                            "fgActEnd += this->num_seq_dims();\n", 
+                                language="cpp", line_no_from=177, style="monokai")
+
+        code_out_gate.scale(code_scale).align_to(code_ex_text, LEFT)
+        code_source = Text("Code directly from source at:\nhttps://github.com/szcom/rnnlib/blob/master/src/LstmLayer.hpp")
+        code_source.scale(.25).align_to(code_ex_text, LEFT).shift(DOWN*3)
+        print(Code.styles_list)
+        self.play(FadeIn(input_arrow),  FadeIn(input_text))
+        self.play(FadeIn(hidden_arrow), FadeIn(hidden_text))
+        self.play(FadeIn(output_arrow), FadeIn(output_text))
+        self.wait(2)
+        self.play(FadeOut(first_rnn))
+        self.play(arrows.animate.shift(RIGHT*3.5), text.animate.shift(RIGHT*3))
+        self.wait(2)
+        self.play(FadeIn(input_exp_text))
+        self.wait(1)
+        self.play(FadeIn(output_exp_text))
+        self.wait(1)
+        self.play(FadeIn(hidden_exp_text))
+        self.wait(3)
+        self.play(FadeOut(arrows), FadeOut(text), FadeOut(exp_text))
+        self.play(Transform(hidden_exp_text, big_hidden_exp_text))
+        self.wait(1)
+        self.play(FadeIn(lstm_cell_img))
+        self.wait(3)
+        self.play(lstm_cell_img.animate.scale(.6).shift(*[UP*2, LEFT*5]))
+        self.wait(1)
+        self.play(Write(math_text), FadeIn(code_ex_text))
+        self.play(FadeIn(code_source), FadeIn(code_out_gate))
+        self.wait(2)
+        self.play(Create(framebox_first_f, rate_func=linear))
+        self.wait(2)
+        self.play(FadeOut(framebox_first_f))
+        self.wait(2)
+        self.play(Create(framebox_input, rate_func=linear))
+        self.wait(2)
+        self.play(ReplacementTransform(framebox_input, framebox_weight))
+        self.wait(2)
+        self.play(ReplacementTransform(framebox_weight, framebox_bias))
 
 
     def intro(self):
@@ -116,7 +311,8 @@ class AnimationMLDL(Scene):
         source_lstm.scale(self.text_source_scale).shift(DOWN*2.5)
         self.play(Write(text_lstm, run_time=2), FadeIn(source_lstm))
         self.wait()
-
+        self.play(FadeOut(text_lstm), FadeOut(source_lstm))
+        self.wait()
 
     def create_initial_text(self):
         text_polito = Text("Politecnico di Torino - Apr 2021")
